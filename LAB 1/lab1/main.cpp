97234e374a6a2977c5b3415815f9858e7308bc95
AlejandraG <maria.gutierrezr1@udea.edu.co>
2025-03-17 18:59:35 -0500

    Ejercicio 7 y 9

--- LAB 1/lab1/main.cpp
+++ LAB 1/lab1/main.cpp
@@ -5,10 +5,12 @@ using namespace std;
 void ejercicio1();
 int ejercicio3();
 int ejercicio5();
+int ejercicio7();
+int ejercicio9();
 
 int main()
 {
-    ejercicio5();
+    ejercicio9();
     return 0;
 }
 
@@ -47,5 +49,35 @@ int ejercicio5(){
     cin >> b;
     dividir = a / b;
     cout << "El resultado de la divison es: " << dividir;
+    return 0;
+}
+
+int ejercicio7(){
+    int n, sum, i;
+
+    sum = 0;
+
+    cout << "Introduce un numero: ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++){
+        sum = sum + i;
+    }
+    cout <<  "la suma es: " << sum << endl;
+    return 0;
+}
+
+int ejercicio9(){
+    int n;
+    double perimetro,area;
+
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    perimetro = 2 * 3.1416 * n;
+    area = 3.1416 * n * n;
+
+    cout << "El perimetro: " << perimetro << endl;
+    cout << "el area es : " << area << endl;
     return 0;
 }
